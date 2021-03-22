38f7b67bec539dde5811174a6112835785246539
Avita India <avitaind@gmail.com>
2021-03-22 16:13:12 +0530

    UI And Changes

--- resources/views/product/accessories/ubique-mouse.blade.php
+++ resources/views/product/accessories/ubique-mouse.blade.php
@@ -1,1 +1,39 @@
-<h1>dsgvhdfnl</h1>
+@extends('layouts.parent')
+
+@section('css')
+
+
+@endsection
+
+@section('content')
+<!-- Content -->
+
+<main class="top-nav-padding">
+    <div class="product-fix-menu">
+    
+          <div class="menu-item">        
+                      
+          <a href="https://in.nexstmall.com/" target="_blank">BUY<br><small>NOW</small></a> 
+          
+         
+          </div>
+    
+          <div class="menu-item">
+             <a href="https://avita-india.com/where-to-buy">Where<br><small>to buy</small></a>
+          </div>
+    
+          </div>
+       <section class="">
+    
+            <img src="{{ asset('assets/images/modus/accessories.png') }}" class="w-100 ">
+    
+        </section>
+    
+    </main>
+    
+    <!-- ./Content -->
+@endsection
+
+@section('scripts')
+
+@endsection
